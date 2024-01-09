2d9cfec0af08b747ca532736d990faa66a73f41a
giapdang <222001442@daihocthudo.edu.vn>
2024-01-09 14:50:05 +0700

    one_commit

--- bt_B5/bai3.js
+++ bt_B5/bai3.js
@@ -1,13 +1,18 @@
 
+function ngaunhien(min,max) {
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
 function ham() {
     var x = document.getElementById("ip1")
     x = Number(x.value)
-    var y = Math.round(Math.random()) // Trả về 0 hoặc 1
-    if (x >= 2 || x < 0) {
+    // var y = Math.round(Math.random()) // Trả về 0 hoặc 1
+    var y = ngaunhien(1,10);
+    if (x > 10 || x < 1) {
         document.getElementById("hp1").innerHTML = "Bạn nhập sai số, vui lòng nhập lại";
     } else if (x == y) {
         document.getElementById("hp1").innerHTML = "Bạn đã đoán đúng";
     } else if (x != y) {
-        document.getElementById("hp1").innerHTML = "Bạn đã đoán sai";
+        document.getElementById("hp1").innerHTML = "Bạn đã đoán sai , số đúng là " + y;
     }
 }
