272194da2a9ff110873af163f16385f8f7813e00
giapdang <222001442@daihocthudo.edu.vn>
2024-01-26 16:14:38 +0700

    contact

---
+++ bt_Ck/contact.js
@@ -0,0 +1,33 @@
+document.getElementById("button1").addEventListener("click", function() {
+    // Reset the transform property to none before starting the animation
+    this.style.transform = "none";
+
+    // Trigger the animation
+    this.style.animation = "rotate 1s linear";
+
+    // Reset the animation property after the animation completes
+    setTimeout(() => {
+        this.style.animation = "";
+    }, 1000);
+});
+
+function send() {
+    var name = document.getElementById("username").value;
+    var email1 = document.getElementById("email").value;
+    var phone1 = document.getElementById("phone").value;
+    var mess = document.getElementById("Mess").value;
+
+    if (name === "" || email1 === "" || phone1 === "") {
+        alert("Vui lòng nhập đầy đủ thông tin");
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("Mess").value = "";
+    } else {
+        alert("Cảm ơn bạn đã gửi thông tin cho chúng tôi");
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("Mess").value = "";
+    }
+}
